34bfd7fe3a5cfcc03acef1fb6dd440447753cc59
Kris Sunu Purnandaru <kris.sunupurnandaru@ai.astra.co.id>
2019-01-22 11:22:06 +0700

    Added Create Quote, Delete Quote, and Progress Bar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,47 @@
 <template>
-    <div class="container">
-    
+  <div class="container">
+    <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+    <app-quote-grid :quotes="quotes" @quoteDeleted="deleteQuote"></app-quote-grid>
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <div class="alert alert-info">Info: Click on a quote to delete it!</div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+  import QuoteGrid from "./components/QuoteGrid";
+  import NewQuote from "./components/NewQuote";
+  import Header from "./components/Header";
+
+  export default {
+    data: function () {
+      return {
+        quotes: [
+          'Just a Quote'
+        ],
+        maxQuotes: 10
+      }
+    },
+    methods: {
+      newQuote(quote) {
+        if(this.quotes.length >= this.maxQuotes ) {
+          return alert('Please delete quote first!');
+        }
+        this.quotes.push(quote);
+      },
+      deleteQuote(index) {
+        this.quotes.splice(index, 1);
+      }
+    },
+    components: {
+      appQuoteGrid: QuoteGrid,
+      appNewQuote: NewQuote,
+      appHeader: Header
     }
+  }
 </script>
 
 <style>
